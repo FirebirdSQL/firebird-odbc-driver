1e9d21686eafb27c936e70f4ec0c7d8cb154bbd9
Vladimir Tsvigun <praktik@users.sourceforge.net>
2003-08-10 17:06:50 +0000

    patch for SQLStatistics according to SQL 92 branch v1-1-beta

--- IscDbc/IscIndexInfoResultSet.cpp
+++ IscDbc/IscIndexInfoResultSet.cpp
@@ -69,7 +69,7 @@ void IscIndexInfoResultSet::getIndexInfo(const char * catalog,
 		"select NULL as table_cat,\n"								// 1
 				"\tNULL as table_schem,\n"							// 2
 				"\trl.rdb$relation_name as table_name,\n"			// 3
-				"\tcast(NULL as smallint) as non_unique,\n"			// 4 
+				"\tcast(0 as smallint) as non_unique,\n"			// 4 
 				"\tcast(NULL as char(31)) as index_qualifier,\n"	// 5 
 				"\tcast(NULL as char(31)) index_name,\n"			// 6
 				"\tcast(0 as smallint) as \"TYPE\",\n"				// 7  SQL_TABLE_STAT
@@ -86,7 +86,7 @@ void IscIndexInfoResultSet::getIndexInfo(const char * catalog,
 		"select NULL as table_cat,\n"								// 1
 				"\tNULL as table_schem,\n"							// 2
 				"\tidx.rdb$relation_name as table_name,\n"			// 3
-				"\tidx.rdb$unique_flag as non_unique,\n"			// 4
+				"\tcast((1-idx.rdb$unique_flag) as smallint) as non_unique,\n"	// 4
 				"\tidx.rdb$index_name as index_qualifier,\n"		// 5
 				"\tidx.rdb$index_name as index_name,\n"				// 6
 				"\tcast(3 as smallint) as \"TYPE\",\n"				// 7 (SQL_INDEX_OTHER)
@@ -100,25 +100,7 @@ void IscIndexInfoResultSet::getIndexInfo(const char * catalog,
 		"from rdb$indices idx, rdb$index_segments seg\n"
 		" where idx.rdb$index_name = seg.rdb$index_name\n";
 
-	const char *preV6 = 
-		"select NULL as table_cat,\n"								// 1
-				"\tNULL as table_schem,\n"							// 2
-				"\tidx.rdb$relation_name as table_name,\n"			// 3
-				"\tidx.rdb$unique_flag as non_unique,\n"			// 4
-				"\tidx.rdb$index_name as index_qualifier,\n"		// 5
-				"\tidx.rdb$index_name as index_name,\n"				// 6
-				"\tseg.rdb$field_position as odbc_type,\n"			// 7 (SQL_INDEX_OTHER)
-				"\tseg.rdb$field_position as ordinal_position,\n"	// 8
-				"\tseg.rdb$field_name as column_name,\n"			// 9				
-				"\tcast(NULL as char) as asc_or_desc,\n"			// 10
-				"\tcast(NULL as integer) as cardinality,\n"			// 11
-				"\tcast(NULL as integer) as \"PAGES\",\n"			// 12
-				"\tcast(NULL as varchar(31)) as filter_condition,\n"// 13
-				"\tidx.rdb$index_type as index_type\n"				// 14
-		"from rdb$indices idx, rdb$index_segments seg\n"
-		" where idx.rdb$index_name = seg.rdb$index_name\n";
-
-	JString sql = (metaData->storesMixedCaseQuotedIdentifiers()) ? v6 : preV6;
+	JString sql = v6;
 
 	if (tableNamePattern && *tableNamePattern)
 	{
@@ -129,7 +111,7 @@ void IscIndexInfoResultSet::getIndexInfo(const char * catalog,
 	if (unique)
 		sql += " and idx.rdb$unique_flag = 1\n";
 
-	sql += " order by 3, 7, 4 desc, 6, 8\n";
+	sql += " order by 4, 7, 5, 6, 8\n";
 	sql = tableStat + "\tunion\n" + sql;
 
 	prepareStatement (sql);
@@ -152,11 +134,10 @@ bool IscIndexInfoResultSet::next()
 
 	int type = resultSet->getInt(7);
 
-	if( type != 0 ) // #define SQL_TABLE_STAT 0
+	if( type == 0 ) // #define SQL_TABLE_STAT 0
+		resultSet->setNull(4);
+	else 
 	{
-		int uniqueFlag = resultSet->getInt (4);
-		resultSet->setValue (4, (uniqueFlag) ? 0 : 1);
-
 		int position = resultSet->getInt(8);
 		resultSet->setValue(8,position+1);
 
