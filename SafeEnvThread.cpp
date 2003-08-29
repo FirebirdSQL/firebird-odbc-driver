10a37cfd8d08eb052b8d2402e1cc3663df79a6d0
Vladimir Tsvigun <praktik@users.sourceforge.net>
2003-08-29 13:03:29 +0000

    no message

--- SafeEnvThread.cpp
+++ SafeEnvThread.cpp
@@ -5,7 +5,7 @@
 #include "OdbcJdbc.h"
 #include "SafeEnvThread.h"
 
-#if(DRIVER_LOCKED_LEVEL == DRIVER_LOCKED_LEVEL_ENV)
+#if(DRIVER_LOCKED_LEVEL == DRIVER_LOCKED_LEVEL_ENV || DRIVER_LOCKED_LEVEL == DRIVER_LOCKED_LEVEL_CONNECT)
 
 void * MutexEnvThread::mutexLockedLevelEnv = NULL;
 
