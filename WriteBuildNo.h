bf0211bf80e3a619f5bb005df19fcdb1e6d64911
Vladimir Tsvigun <praktik@users.sourceforge.net>
2004-02-17 19:21:46 +0000

    - current BUILDNUM_VERSION set 40 branch v1-2-beta

--- WriteBuildNo.h
+++ WriteBuildNo.h
@@ -1,6 +1,4 @@
 // number of entering of updatings in the current version
 // for the new version new readout
 // DIG number
-#define BUILDNUM_VERSION 	0039
-// HEX number
-#define BUILDHEX_VERSION 	0x0027
+#define BUILDNUM_VERSION	40
