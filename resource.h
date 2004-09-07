64896bc820343b994153a5e8a5d18424df69c60e
Vladimir Tsvigun <praktik@users.sourceforge.net>
2004-09-07 19:29:39 +0000

    - added ID for translate strings (resource.h)

--- resource.h
+++ resource.h
@@ -8,3 +8,13 @@
 #define IDC_USER                        1001
 #define IDC_PASSWORD                    1002
 #define IDC_ROLE                        1003
+
+//
+// ID for translate strings
+//
+#define IDS_DLG_TITLE_CONNECT		0
+#define IDS_BUTTON_OK			1
+#define IDS_BUTTON_CANCEL		2
+#define IDS_STATIC_ACCOUNT		3
+#define IDS_STATIC_PASSWORD		4
+#define IDS_STATIC_ROLE			5
