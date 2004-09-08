b8123a02f6e76af008730f3ecb768d01d32cd4ef
Vladimir Tsvigun <praktik@users.sourceforge.net>
2004-09-08 08:46:37 +0000

    - change link for license IDPL

--- IscDbc/SqlTime.h
+++ IscDbc/SqlTime.h
@@ -4,7 +4,7 @@
  *     Developer's Public License Version 1.0 (the "License"); 
  *     you may not use this file except in compliance with the 
  *     License. You may obtain a copy of the License at 
- *     http://www.ibphoenix.com/idpl.html. 
+ *     http://www.ibphoenix.com/main.nfs?a=ibphoenix&page=ibp_idpl.
  *
  *     Software distributed under the License is distributed on 
  *     an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either 
