f0a2c86cdc16f50c5cf38fd922d4b4bb3e468dd4
Vladimir Tsvigun <praktik@users.sourceforge.net>
2006-01-22 18:48:28 +0000

    - added new source files For reception of the information about an belonging   of a field to a primary key,  branch v2-0-beta

---
+++ IscDbc/IscColumnKeyInfo.h
@@ -0,0 +1,41 @@
+/*
+ *  
+ *     The contents of this file are subject to the Initial 
+ *     Developer's Public License Version 1.0 (the "License"); 
+ *     you may not use this file except in compliance with the 
+ *     License. You may obtain a copy of the License at 
+ *     http://www.ibphoenix.com/main.nfs?a=ibphoenix&page=ibp_idpl.
+ *
+ *     Software distributed under the License is distributed on 
+ *     an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either 
+ *     express or implied.  See the License for the specific 
+ *     language governing rights and limitations under the License.
+ *
+ *
+ *  The Original Code was created by Vladimir Tsvigun for IBPhoenix.
+ *
+ *  Copyright (c) 2006 Vladimir Tsvigun
+ *  All Rights Reserved.
+ */
+
+// IscColumnKeyInfo.h: interface for the IscColumnKeyInfo class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(_IscColumnKeyInfo_h_)
+#define _IscColumnKeyInfo_h_
+
+#include "IscMetaDataResultSet.h"
+
+namespace IscDbcLibrary {
+
+class IscColumnKeyInfo : public IscMetaDataResultSet  
+{
+public:
+	virtual bool getColumnKeyInfo( const char *tableName, const char *columnName );
+	IscColumnKeyInfo( IscDatabaseMetaData *metaData );
+};
+
+}; // end namespace IscDbcLibrary
+
+#endif // !defined(_IscColumnKeyInfo_h_)
