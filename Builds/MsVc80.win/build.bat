9af253e5b58e52fc13f6d71693f7bf7a2af84b70
Paul Reeves <paul_reeves@users.sourceforge.net>
2007-04-25 13:24:24 +0000

    Add support to build x64 targets from msvc8 makefile

--- Builds/MsVc80.win/build.bat
+++ Builds/MsVc80.win/build.bat
@@ -1,7 +1,23 @@
-@rem 
-@rem Load VCVARS32.BAT from "C:\Program Files\Microsoft Visual Studio 8\VC\bin\"
-@rem Create d:/Firebird/include and copy ibase.h iberror.h 
-@rem Run this file
-@rem
-@set COMPDIR=%VCINSTALLDIR%
-@"%VCINSTALLDIR%\Bin"\nmake -f makefile.msvc8 %1
+@echo off
+call setenvvar.bat 8
+::if errorlevel 1 (@echo errorlevel is %errorlevel%) & (goto :EOF)
+
+@set COMPDIR="%VCINSTALLDIR%"
+
+if "%FB_TARGET_PLATFORM%"=="win32" (
+  if defined FBODBC_LOG (
+    @"%VCINSTALLDIR%\Bin"\nmake  /d /p -f makefile.msvc8 %1 > nmake_win32.log 2>&1
+  ) else
+  @"%VCINSTALLDIR%\Bin"\nmake  -f makefile.msvc8 %1
+  )
+) else (
+  if defined FBODBC_LOG (
+    @"%VCINSTALLDIR%\Bin"\amd64\nmake  /d /p -f makefile.msvc8 %1 > nmake_amd64.log 2>&1
+  ) else
+    @"%VCINSTALLDIR%\Bin"\amd64\nmake  -f makefile.msvc8 %1
+  )
+)
+
+goto :EOF
+
+
