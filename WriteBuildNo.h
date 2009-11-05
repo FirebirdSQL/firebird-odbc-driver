31cfab266a8896d386a324e4f689eea836e77854
Paul Reeves <paul_reeves@users.sourceforge.net>
2009-11-05 09:43:31 +0000

    Add tab and document reason

--- WriteBuildNo.h
+++ WriteBuildNo.h
@@ -1,4 +1,7 @@
 // number of entering of updatings in the current version
 // for the new version new readout
 // DIG number
-#define BUILDNUM_VERSION	149
+// Note - there must be two tabs between BUILDNUM_VERSION and 
+// the actual number, otherwise the makefile for linux will not
+// pick up the value.
+#define BUILDNUM_VERSION		149
