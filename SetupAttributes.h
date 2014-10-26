a8c0a0983cd815a9076234e9887754bea1d7e09f
Alexander Potapchenko <alexander.potapchenko@gmail.com>
2014-10-26 11:33:51 +0000

    update build number

--- SetupAttributes.h
+++ SetupAttributes.h
@@ -28,7 +28,7 @@
 #define BUILDTYPE_VERSION 	"WI_T"
 #define MAJOR_VERSION 		2
 #define MINOR_VERSION 		0
-#define REVNO_VERSION 		2
+#define REVNO_VERSION 		3
 
 #ifdef _WIN64
 #define SUFFIX_BUILD		"64"
